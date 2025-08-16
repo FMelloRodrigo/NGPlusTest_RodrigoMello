f3671ac395d96c67705f32c4069894cf7319eca1
Rodrigo <rodrigomello.unrealmarketplace@gmail.com>
2025-08-16 10:39:12 -0300

    Skate Mesh Import and Initial Setup on the Character

--- Source/NGSkate/NGSkateCharacter.h
+++ Source/NGSkate/NGSkateCharacter.h
@@ -5,6 +5,7 @@
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "Logging/LogMacros.h"
+#include "Components/StaticMeshComponent.h"
 #include "NGSkateCharacter.generated.h"
 
 class USpringArmComponent;
@@ -20,29 +21,39 @@ class ANGSkateCharacter : public ACharacter
 {
 	GENERATED_BODY()
 
-	/** Camera boom positioning the camera behind the character */
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	USpringArmComponent* CameraBoom;
 
-	/** Follow camera */
+	
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	UCameraComponent* FollowCamera;
 	
-	/** MappingContext */
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputMappingContext* DefaultMappingContext;
 
-	/** Jump Input Action */
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* JumpAction;
 
-	/** Move Input Action */
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* MoveAction;
 
-	/** Look Input Action */
+	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
 	UInputAction* LookAction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	UInputAction* EnterSkateAction;
+
+	//Skate Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = SkateComponents, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* PhysicsBallMesh;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = SkateComponents, meta = (AllowPrivateAccess = "true"))
+	UStaticMeshComponent* SkateMesh;
 
 public:
 	ANGSkateCharacter();
@@ -50,11 +61,13 @@ public:
 
 protected:
 
-	/** Called for movement input */
+	
 	void Move(const FInputActionValue& Value);
 
-	/** Called for looking input */
+	
 	void Look(const FInputActionValue& Value);
+
+	void EnterSkateInput();
 			
 
 protected:
@@ -64,9 +77,9 @@ protected:
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 public:
-	/** Returns CameraBoom subobject **/
+	
 	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
-	/** Returns FollowCamera subobject **/
+	
 	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
 };
 
